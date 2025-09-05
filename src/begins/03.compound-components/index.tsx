0bb8663ac9e7554f4c659e496fb2bc6f2185c56e
vietdan1613 <viet.dan.1613@gmail.com>
2025-09-05 15:52:17 +0700

    update react pattern

--- src/begins/03.compound-components/index.tsx
+++ src/begins/03.compound-components/index.tsx
@@ -1,13 +1,58 @@
-import { Toggle, ToggleOn, ToggleOff, ToggleButton } from "./toggle.tsx";
+import { type SportData, type User } from "../../shared/types.tsx";
+import { useState } from "react";
+import { allSports } from "./data/sportsData.tsx";
+import { Footer } from "./Footer.tsx";
+import { Nav } from "./Nav.tsx";
+import Main from "./main";
+import { Details } from "./main/Details.tsx";
+import { List } from "./main/List.tsx";
+import { SportListItemButton } from "./main/SportListItemButton.tsx";
 
-export function App() {
+function App() {
+  const [user] = useState<User>({ name: "Pika", image: "/img/pikachu.png" });
+  const [sportList] = useState<Array<SportData>>(() =>
+    Object.values(allSports)
+  );
+  const [selectedSport, setSelectedSport] = useState<SportData | null>(null);
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
   return (
-    <div>
-      <Toggle>
-        <ToggleOn>The button is on</ToggleOn>
-        <ToggleOff>The button is off</ToggleOff>
-        <ToggleButton />
-      </Toggle>
+    <div
+      id="app-root"
+      className={`min-h-screen text-gray-800 ${isDarkTheme ? "bg-gray-500" : "bg-gray-50"}`}
+      style={{
+        ["--accent-color" as keyof React.CSSProperties]:
+          selectedSport?.color ?? "black",
+      }}
+    >
+      <div className="flex flex-col min-h-screen">
+        <Nav
+          theme={isDarkTheme}
+          setTheme={setIsDarkTheme}
+          avatar={<img
+            src={user.image}
+            alt={`${user.name} profile`}
+            className="w-full h-full object-cover"
+          />}
+        />
+        <Main
+          list={<List
+            sportList={
+              sportList.map((p) => (
+                <li key={p.id}>
+                  <SportListItemButton
+                    sport={p}
+                    onClick={() => setSelectedSport(p)}
+                  />
+                </li>
+              ))}
+          />}
+          content={<Details selectedSport={selectedSport} />}
+        />
+        <Footer message={<p>
+          Don't have a good day–have a great day,{" "}
+          <span className="font-semibold">{user.name}</span>
+        </p>} />
+      </div>
     </div>
   );
 }
---
+++ src/begins/03.compound-components/main/Details.tsx
@@ -0,0 +1,39 @@
+import { SportDataView } from "../data/SportDataView";
+import { type SportData } from "../data/types.tsx";
+import { useState } from "react";
+
+export function Details({ selectedSport }: { selectedSport: SportData | null }) {
+    const [enabled, setEnabled] = useState(true);
+
+    return (
+        <div>
+            <ToggleButton enabled={enabled} setEnabled={setEnabled} />
+            <div className="py-6 mt-4 bg-white rounded-lg shadow border border-gray-200 min-h-[200px] flex items-center justify-center w-full"
+                style={{
+                    display: enabled ? 'block' : 'none'
+                }}
+            >
+                {selectedSport ? (
+                    <SportDataView sport={selectedSport} />
+                ) : (
+                    <div className="text-gray-500 italic"></div>
+                )}
+            </div>
+        </div>
+    );
+}
+
+function ToggleButton({ enabled, setEnabled }: { enabled: boolean, setEnabled: (value: boolean) => void }) {
+    return (
+        <button
+            onClick={() => setEnabled(!enabled)}
+            className={`relative inline-flex h-6 w-11 items-center rounded-full transition-colors ${enabled ? "bg-blue-600" : "bg-gray-300"
+                }`}
+        >
+            <span
+                className={`inline-block h-4 w-4 transform rounded-full bg-white transition-transform ${enabled ? "translate-x-6" : "translate-x-1"
+                    }`}
+            />
+        </button>
+    );
+}
